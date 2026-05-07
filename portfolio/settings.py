41e4a1091c3bf9519009d9c7e7683ec477d1a79e
Azamat Jumabayev <azamatjumabayev8774@gmail.com>
2026-05-07 11:12:52 +0500

    Bug togirlandi

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-this-in-production-use-env-variables'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
